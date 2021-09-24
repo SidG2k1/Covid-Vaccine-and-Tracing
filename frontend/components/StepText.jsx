caaac13980fc0add89e540507ba5a70635b31485
zhu-tom <zhutom01@gmail.com>
2021-09-24 13:55:08 -0400

    add step text

---
+++ frontend/components/StepText.jsx
@@ -0,0 +1,10 @@
+import styledComponentsNativeCjs from "styled-components/native";
+
+export default styledComponentsNativeCjs.Text`
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 17px;
+  color: #F68972;
+`
