3ec871e94feb0c16d74fa8e5ec219f5227c9c6bf
Khang Nguyen <nguyenh.khang.1999@gmail.com>
2021-09-24 14:16:59 -0400

    added drop down component and location screen

--- frontend/Screens/onboard-1-2.jsx
+++ frontend/Screens/onboard-1-2.jsx
@@ -3,6 +3,7 @@ import Title from "../components/Title";
 import SecondaryButton from "../components/SecondaryButton";
 import DropDown from "../components/DropDown"
 import Subtitle from "../components/Subtitle"
+import { View, Image, StyleSheet } from 'react-native';
 
 export const onboard12Screen = ({ navigation, route }) => {
     const PROVINCES = ["Alberta",
@@ -20,10 +21,14 @@ export const onboard12Screen = ({ navigation, route }) => {
         "Yukon"]
     return (
         <React.Fragment>
-            <Title style={{ marginTop: 170, marginBottom: 70 }}>Location</Title>
-            <Subtitle>For the most up-to-date experience with covid-19 restrictions ...</Subtitle>
+            
+            <Image style={{height:155, width:203, alignSelf:"center", marginTop:100}} source={require('../images/2.png')}/>
+            <Title style={{marginBottom: 20, marginTop: 50 }}>Location</Title>
+            <Subtitle style={{fontSize:12}}>For the most up-to-date experience with covid-19 restrictions ...</Subtitle>
             <div style={{ marginTop: 50 }}>
                 <div style={{ marginBottom: 50 }}>
+                    
+            <Subtitle style={{}}>Province:</Subtitle>
                 <DropDown values={PROVINCES}/>
                 </div>
                 <SecondaryButton
