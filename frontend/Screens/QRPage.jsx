c41b19360e1f176363b5f936129977140723a16b
Khang Nguyen <nguyenh.khang.1999@gmail.com>
2021-09-24 16:05:10 -0400

    added apple wallet

--- frontend/Screens/QRPage.jsx
+++ frontend/Screens/QRPage.jsx
@@ -4,6 +4,7 @@ import Symptom from "../components/Symptom";
 import SecondaryButton from "../components/SecondaryButton";
 import QRCode from 'react-native-qrcode-svg';
 import DropDown from "../components/DropDown"
+import { View, Image, StyleSheet } from 'react-native';
 import Subtitle from "../components/Subtitle"
 
 export const QRPage = ({ navigation, route }) => {
@@ -19,15 +20,15 @@ export const QRPage = ({ navigation, route }) => {
             </div>
             <div style={{ marginTop: 50 }}>
                 <div style={{ marginBottom: 50 }}>
+                    <Image source={require('../images/wallet.png')} style={{ width: 320, height: 99 }} />
+                </div>
+                <div style={{ marginBottom: 50 }}>
 
                     <Subtitle style={{}}>Save to:</Subtitle>
                     <DropDown values={["Photos", "Files", "Google Drive"]} />
                 </div>
                 <SecondaryButton
-                    title="Next"
-                    onPress={() =>
-                        navigation.navigate('onboard-2', route.params)
-                    }
+                    title="Save"
                 />
             </div>
         </React.Fragment>
